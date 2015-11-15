cb1b7809b24853307c05808cedd5036ecc408257
RehabMan <racerrehabman@gmail.com>
2015-11-15 09:43:57 -0800

    fix bug with zero length dictionary

--- AppleSmartBatteryManager/AppleSmartBatteryManager.cpp
+++ AppleSmartBatteryManager/AppleSmartBatteryManager.cpp
@@ -549,7 +549,9 @@ OSObject* AppleSmartBatteryManager::translateArray(OSArray* array)
             return NULL;
 
         // dictionary constructed to accomodate all pairs
-        OSDictionary* dict = OSDictionary::withCapacity(count >> 1);
+        int size = count >> 1;
+        if (!size) size = 1;
+        OSDictionary* dict = OSDictionary::withCapacity(size);
         if (!dict)
             return NULL;
 
